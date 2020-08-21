d9ab2a005eebc4c811700b1a7cc8299f5c7e74e5
tony <tsainez@gmail.com>
2020-08-21 04:53:50 -0700

    Update waveform.m

--- Simulation/waveform.m
+++ Simulation/waveform.m
@@ -1,3 +1,12 @@
 % Mexican hat wavelet is chosen as the mother wavelet as it is a good
 % representation of the footstep-induced vibration signals due to its similarity in shape.
 
+wname = 'mexh'; % See https://www.mathworks.com/help/wavelet/ref/waveinfo.html
+itr = 10;
+
+[psi,xval] = wavefun(wname,itr);
+plot(xval,psi)
+grid on
+title(['Approximation of ',wname,' Wavelet'])
+
+% For more information, open openExample('wavelet/WaveletApproximationsExample')
